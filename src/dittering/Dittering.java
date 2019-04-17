17d76a7bbab2cbacfbc87277dcba2baef6ae6375
Bfraeyma <Benjamin.Fraeyman@UGent.be>
2019-04-17 13:37:36 +0200

    Show image possible

--- src/dittering/Dittering.java
+++ src/dittering/Dittering.java
@@ -4,25 +4,28 @@
  * and open the template in the editor.
  */
 package dittering;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
+//import java.awt.image.BufferedImage;
+//import javax.imageio.ImageIO;
+//import java.io.File;
+//import java.io.IOException;
+//import javax.swing.JFileChooser;
+//import java.io.Writer;
+//import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.awt.Graphics;
+//import javax.swing.JFrame;
 /**
  *
  * @author pollepel
  */
-public class Dittering {
-    private BufferedImage img;
+public class Dittering/* extends JFrame*/{
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File("strawberry.jpg"));
-        } catch (IOException e) {
-        }
+        Image image = new Image();
+        image.openAFile();
+        image.showImage();
+        image.setVisible(true);
     }
-    
 }
